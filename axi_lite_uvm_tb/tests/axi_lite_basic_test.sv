dbb690a28d476df46ac84d091b19534f573eb47e
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2025-07-13 10:11:28 -0700

    Create axi_lite_basic_test.sv

---
+++ axi_lite_uvm_tb/tests/axi_lite_basic_test.sv
@@ -0,0 +1,16 @@
+class axi_lite_basic_test extends uvm_test;
+  `uvm_component_utils(axi_lite_basic_test)
+
+  axi_lite_monitor mon;
+  axi_lite_checker check;
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    mon = axi_lite_monitor::type_id::create("mon", this);
+    check = axi_lite_checker::type_id::create("check", this);
+  endfunction
+endclass
