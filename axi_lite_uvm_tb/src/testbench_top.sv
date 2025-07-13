c50f430a25cd03453ab9c7b967fae8f5468dee4c
yegane-AI <51883127+yegane-AI@users.noreply.github.com>
2025-07-13 10:10:38 -0700

    Create testbench_top.sv

---
+++ axi_lite_uvm_tb/src/testbench_top.sv
@@ -0,0 +1,24 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+import axi_lite_pkg::*;
+
+module testbench_top;
+
+  logic ACLK = 0;
+  logic ARESETn = 0;
+
+  always #5 ACLK = ~ACLK;
+
+  axi_lite_if #(64) axi_if(.*);
+
+  initial begin
+    ARESETn = 0;
+    #20 ARESETn = 1;
+  end
+
+  initial begin
+    uvm_config_db#(virtual axi_lite_if)::set(null, "*", "vif", axi_if);
+    run_test("axi_lite_basic_test");
+  end
+
+endmodule
